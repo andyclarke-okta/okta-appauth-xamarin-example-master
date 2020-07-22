2e7214ff16e083e8e32211fd1ebef8ce68e7c149
Andy Clarke <andy.clarke@okta.com>
2020-07-22 11:48:05 -0400

    Add project files.

---
+++ OktaDemo.XF/OktaDemo.XF/Helpers/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace OktaDemo.XF.Helpers
+{
+    public class Constants
+    {
+        public const string AuthStateKey = "authState";
+        public const string AuthServiceDiscoveryKey = "authServiceDiscovery";
+
+        public const string ClientId = "0oaowx94seN1iPJHY0h7";
+        public const string RedirectUri = "com.oktapreview.aclarke:/callback";
+        public const string PostLogoutRedirectUri = "com.oktapreview.aclarke:/logoutCallback";
+        public const string OrgUrl = "https://aclarke.oktapreview.com";
+        public const string AuthorizationServerId = "default";
+
+        public static readonly string DiscoveryEndpoint =
+            $"{OrgUrl}/oauth2/{AuthorizationServerId}/.well-known/openid-configuration";
+
+
+        public static readonly string[] Scopes = new string[] {
+            "openid", "profile", "email", "offline_access" };
+    }
+}
---
+++ OktaDemo.XF/OktaDemo.XF/Pages/AuthInfoPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OktaDemo.XF.Interfaces;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OktaDemo.XF.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthInfoPage : ContentPage
+    {
+        public AuthInfoPage(string name, string email, string preferredUsername)
+        {
+            InitializeComponent();
+            _nameLabel.Text = name;
+            _emailLabel.Text = email;
+            _preferredUsernameLabel.Text = preferredUsername;
+        }
+
+        private async void LogoutButton_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("LogoutButton_Clicked");
+            var loginProvider = DependencyService.Get<ILoginProvider>();
+            IsBusy = true;
+            await loginProvider.LogoutAsync();
+
+            await Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
